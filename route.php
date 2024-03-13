eaacdc35a73b39d4140e7b2ab27fcb3b624c7ffa
SuperCodeStajyer <stajyer@supercode.com.tr>
2024-03-13 09:03:47 +0300

    .

--- route.php
+++ route.php
@@ -2,15 +2,15 @@
 $request_uri = $_SERVER['REQUEST_URI'];
 
 $routes = [
-    '/' => 'deneme'
+    '/library/' => 'deneme'
 ];
 
 $path = parse_url($request_uri, PHP_URL_PATH);
 
 if (array_key_exists($path, $routes)) {
     $controllerFile = $routes[$path] . '.controller.php';
-    if (file_exists('controllers/' . $controllerFile)) {
-        include('controllers/' . $controllerFile);
+    if (file_exists('controller/' . $controllerFile)) {
+        include('controller/' . $controllerFile);
     } else {
         echo '404 Sayfa Bulunamadı';
     }
