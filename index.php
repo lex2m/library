33534a763970cb6f73ed70c21e18d05c39862780
SuperCodeStajyer <stajyer@supercode.com.tr>
2024-03-13 09:20:33 +0300

    9:20

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 require "function.php";
 require "route.php";
-
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +11,7 @@ require "route.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kütüphane</title>
+<<<<<<< HEAD
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
@@ -19,6 +19,10 @@ require "route.php";
         crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/f30e8168c4.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
+=======
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+>>>>>>> c7d3082 (9:20)
 </head>
 
 <body>
--- route.php
+++ route.php
@@ -2,17 +2,17 @@
 $request_uri = $_SERVER['REQUEST_URI'];
 
 $routes = [
-    '/library/' => 'deneme',
-    '/library/login' => '/Auth/Login',
-    '/library/register' => '/Auth/Register',
+    "/library/" => 'deneme',
+    "/login/" => '/Auth/Login',
+    "/library/register" => '/Auth/Register'
 ];
 
 $path = parse_url($request_uri, PHP_URL_PATH);
 
 if (array_key_exists($path, $routes)) {
     $controllerFile = $routes[$path] . '.controller.php';
-    if (file_exists('Controller/' . $controllerFile)) {
-        include('Controller/' . $controllerFile);
+    if (file_exists('./Controller/' . $controllerFile)) {
+        include('./Controller/' . $controllerFile);
     } else {
         echo '404 Sayfa Bulunamadı';
     }
